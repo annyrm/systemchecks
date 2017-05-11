33b9fd22cee79b18ad1d2ad97f1d8abd7924f8a9
Ana Teresa <ana_teresa30@hotmail.com>
2017-05-11 13:20:12 -0500

    Heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-204-1-40.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dedv5f04pdsej9'),
+            'username' => env('DB_USERNAME', 'xzxtdznrtykfgh'),
+            'password' => env('DB_PASSWORD', '62f64fb0bf278b4902378fa71b954934673f0c694ff5b8804f09ac1dae4b970b'),
+            'uri' => env('DATABASE_URL', 'postgres://xzxtdznrtykfgh:62f64fb0bf278b4902378fa71b954934673f0c694ff5b8804f09ac1dae4b970b@ec2-54-204-1-40.compute-1.amazonaws.com:5432/dedv5f04pdsej9'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
